42ef2dbaf991990b4659d854f453a51af533d476
Rozandi Hikmah <sangpnikmat@gmail.com>
2025-04-13 00:13:02 +0700

    Initial Files2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import AudioPlayer from '../../public/components/AudioPlayer';
-
+import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <AudioPlayer />
         <div className="max-w-sm rounded-4xl overflow-hidden shadow-md shadow-gray-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 animate-wiggle">
-          <img className="w-full" src="/img/bg-dasar.jpg" alt="Sunset in the mountains"></img>
+          <Image className="w-full" src="/img/bg-dasar.jpg" width={500} height={200} alt="Home"></Image>
           <div className="px-6 py-4 text-center bg-gray-900">
             <div className="text-gray-300 font-bold text-xl mb-2">Hello Human</div>
             <p className="text-gray-300 text-base">"If you see this, believe me — you will die. That’s my promise."</p>
